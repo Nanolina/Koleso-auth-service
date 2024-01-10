62bdddaf8a43bfb47746b92c91aafaeac191c9c2
Alina <alinka.12.12.98@gmail.com>
2024-01-10 18:17:41 +0400

    feat: add register module
    create new endpoint to register user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { RegisterModule } from './register/register.module';
 
 @Module({
+  imports: [RegisterModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: process.env.SELLER_INTERFACE_URL,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
