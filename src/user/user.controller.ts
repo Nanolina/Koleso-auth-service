09f0ab2c1654c0b735dcf3a1b66ad55a336aaa73
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-01-10 22:12:03 +0400

    feat: send token in the HttpOnly-cookies for safe keeping

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,7 +5,9 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { JWTService } from '../jwt/jwt.service';
 import { RegisterDto } from './register.dto';
 import { UserService } from './user.service';
@@ -19,13 +21,24 @@ export class UserController {
 
   @Post('/register')
   @HttpCode(HttpStatus.CREATED)
-  async register(@Body() registerDto: RegisterDto) {
+  async register(
+    @Body() registerDto: RegisterDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
     try {
       const user = await this.userService.register(registerDto);
       const token = await this.jwtService.createToken(user);
-      return { token };
+      response.cookie('token', token, {
+        httpOnly: true,
+        path: '/',
+        maxAge: 3600000, // cookie lifetime = 1 hour
+        // secure: true, // uncomment in the production environment for HTTPS
+        sameSite: 'strict',
+      });
+
+      return true;
     } catch (error) {
-      throw new ForbiddenException('Error during registration');
+      throw new ForbiddenException('Error during registration', error.message);
     }
   }
 }
